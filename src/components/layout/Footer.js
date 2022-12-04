31a30bd19e12721120a3ad718f673ebcb97e9caa
Azad Singhal <103386936+singhalazad@users.noreply.github.com>
2022-12-04 13:04:53 +0530

    Update Footer.js
    okk

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,7 +3,7 @@ const footerYear = new Date().getFullYear();
 const Footer = () => {
     return <footer className='footer p-10 bg-gray-700 text-primary-content footer-center'>
         <div>
-            <p>Copyright &copy; {footerYear} All rights reserved to Singhal Azad</p>
+            <p>Assignment for the Digi-K-Labs</p>
         </div>
 
     </footer>;
